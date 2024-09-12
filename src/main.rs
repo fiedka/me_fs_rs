3ff73d646ad13d321410f0b07def33cd6d22f72d
Daniel Maslowski <info@orangecms.org>
2024-09-12 17:47:15 +0200

    print entries with their end and sort CPD entries

--- src/main.rs
+++ src/main.rs
@@ -18,34 +18,40 @@ fn print_directories(cpds: &Vec<CodePartitionDirectory>) {
         let CodePartitionDirectory { header, entries } = cpd;
         let pname = std::str::from_utf8(&header.part_name).unwrap();
         let checksum = header.version_or_checksum;
-        println!("{pname}  {checksum:08x}");
-        println!("  file name        offset    size        compression flags");
+        println!("{pname}  checksum or version: {checksum:08x}");
+        println!("  file name        offset    end       size           compression flags");
+        let mut entries = entries.clone();
+        entries.sort_by_key(|e| e.offset & 0xffffff);
         for e in entries {
-            let o = e.offset;
+            // TODO: See https://github.com/skochinsky/me-tools class CPDEntry
+            // What is the other u8?!
+            let o = e.offset & 0xffffff;
             let s = e.size;
+            let end = o + s;
             let f = e.compression_flag;
             if let Ok(n) = std::str::from_utf8(&e.name) {
                 let n = n.trim_end_matches(char::from(0));
-                println!("  {n:13} @ {o:08x}:{s:08x}    {f:032}");
+                println!("  {n:13} @ 0x{o:06x}:0x{end:06x} (0x{s:06x}) {f:032b}");
             }
         }
     }
 }
 
 fn print_fpt_entries(entries: &Vec<FPTEntry>) {
-    println!("  name     offset     size      type   notes");
+    println!("  name     offset     end         size      type   notes");
     for e in entries {
         let o = e.offset as usize;
         let s = e.size as usize;
+        let end = o + s;
 
         let name = std::str::from_utf8(&e.name).unwrap();
         let name = name.trim_end_matches(char::from(0));
 
         let (part_type, full_name) = me_fs_rs::get_part_info(name);
         let part_info = format!("{part_type:?}: {full_name}");
-        let name_offset_size = format!("{name:>4} @ 0x{o:08x}:0x{s:08x}");
+        let name_offset_end_size = format!("{name:>4} @ 0x{o:08x}:0x{end:08x} (0x{s:08x})");
 
-        println!("- {name_offset_size}  {part_info}");
+        println!("- {name_offset_end_size}  {part_info}");
     }
 }
 

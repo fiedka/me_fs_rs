b700ac07590445845955ed4174e8928a573a3870
지지 ᚠד (Jiji Freya Daniel) Maslowski <info@orangecms.org>
2026-08-10 17:33:54 +0200

    scripts
    Signed-off-by: 지지 ᚠד (Jiji Freya Daniel) Maslowski <info@orangecms.org>

---
+++ certs_8.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+ls $@ \
+  xdump_8_c4b50ce36e08ab51e8158ea2d1bc78dbd5d146c5/004800_*.bin \
+  xdump_8_c4b50ce36e08ab51e8158ea2d1bc78dbd5d146c5/014800_*.bin \
+  xdump_8_c4b50ce36e08ab51e8158ea2d1bc78dbd5d146c5/024800_*.bin \
+  xdump_8_c4b50ce36e08ab51e8158ea2d1bc78dbd5d146c5/034800_*.bin \
+  xdump_8_c4b50ce36e08ab51e8158ea2d1bc78dbd5d146c5/044800_*.bin \
+  xdump_8_c4b50ce36e08ab51e8158ea2d1bc78dbd5d146c5/054800_*.bin \
+  xdump_8_c4b50ce36e08ab51e8158ea2d1bc78dbd5d146c5/064800_*.bin \
+  xdump_8_c4b50ce36e08ab51e8158ea2d1bc78dbd5d146c5/074800_*.bin \
+  xdump_8_c4b50ce36e08ab51e8158ea2d1bc78dbd5d146c5/084800_*.bin \
+  xdump_8_c4b50ce36e08ab51e8158ea2d1bc78dbd5d146c5/094800_*.bin \
+  xdump_8_c4b50ce36e08ab51e8158ea2d1bc78dbd5d146c5/0a4800_*.bin \
+  xdump_8_c4b50ce36e08ab51e8158ea2d1bc78dbd5d146c5/0b4800_*.bin \
+  xdump_8_c4b50ce36e08ab51e8158ea2d1bc78dbd5d146c5/0c4800_*.bin \
+  xdump_8_c4b50ce36e08ab51e8158ea2d1bc78dbd5d146c5/0d4800_*.bin \
+  xdump_8_c4b50ce36e08ab51e8158ea2d1bc78dbd5d146c5/0e4800_*.bin
